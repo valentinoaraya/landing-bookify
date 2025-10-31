bb3c485b5c07144953120339ebc62c24769493e4
nachopatinoc <ignaciocorreagabbi@gmail.com>
2025-10-30 22:07:58 -0300

    video tutorial insertado

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,30 +1,27 @@
 import "./Hero.css"
 import Title from "../../common/Title/Title";
 import Button from "../../common/Button/Button";
-import videoSchedulingAppointment from "../../assets/videos/pidiendo-turno-opt.mp4"
 
 const Hero = () => {
     return (
         <div className="heroContainer">
-
-                <Title>
-                    Agendá, cobrales y notificá… <span className="highlight">todo en un solo lugar</span>
-                </Title>
-
-                <p className="pHero">Configurá tus turnos, compartí un link con tus clientes y empezá a recibir reservas. Todo desde una sola plataforma.</p>
-                
-                <Button
-                    width="300px"
-                    onSubmit={() => window.open("https://www.instagram.com/aedes.tech/", '_blank')}
-                >
-                    Comenzá Ahora
-                </Button>
-                <div className="heroImage">
-                        <video autoPlay loop muted playsInline width={"100%"}>
-                            <source src={videoSchedulingAppointment} type="video/mp4" />
-                            Tu navegador no soporta videos
-                        </video>
-                </div>
+            <Title>
+                Agendá, cobrales y notificá… <span className="highlight">todo en un solo lugar</span>
+            </Title>
+            <p className="pHero">Configurá tus turnos, compartí un link con tus clientes y empezá a recibir reservas. Todo desde una sola plataforma.</p>
+            <Button
+                width="300px"
+                onSubmit={() => window.open("https://www.instagram.com/aedes.tech/", '_blank')}
+            >
+                Comenzá Ahora
+            </Button>
+            <div className="heroImage" style={{ position: 'relative', paddingBottom: '56.25%', height: 0 }}>
+                <iframe 
+                    src="https://www.loom.com/embed/a6d20e360dd841b49e9066900202780e" 
+                    allowFullScreen
+                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                />
+            </div>
         </div>
     );
 }
